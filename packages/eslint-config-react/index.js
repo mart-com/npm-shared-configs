01de062466c2b1ad0829a90abf190f9ebda77a69
Ibrahim AshShohail <me@ibrasho.com>
2019-11-10 07:15:43 +0300

    chore: update package.json files
    Signed-off-by: Ibrahim AshShohail <me@ibrasho.com>

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,9 +1,9 @@
 module.exports = {
+  extends: ['@mart-com', 'plugin:react/recommended'],
+  plugins: ['react'],
   env: {
     browser: true,
-    node: true,
   },
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
@@ -11,22 +11,6 @@ module.exports = {
       jsx: true,
     },
   },
-  overrides: [
-    {
-      files: ['**/*.test.js', '**/*.test.jsx', '**/*.spec.js', '**/*.spec.jsx'],
-      env: {
-        jest: true,
-      },
-    },
-  ],
-  extends: [
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'eslint:recommended',
-    'prettier',
-    'plugin:react/recommended',
-  ],
-  plugins: ['react', 'prettier', 'import', 'jest'],
   settings: {
     react: {
       version: 'detect',
@@ -35,10 +19,5 @@ module.exports = {
   rules: {
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
-    'import/no-unresolved': [2, { commonjs: true, amd: true }],
-    'import/named': 2,
-    'import/namespace': 2,
-    'import/default': 2,
-    'import/export': 2,
   },
 };
