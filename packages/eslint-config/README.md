7dee6949c5934b82511cf6e31d2f293499459b69
Ibrahim AshShohail <me@ibrasho.com>
2019-11-10 07:08:11 +0300

    feat: add base @mart-com/eslint-config package
    Signed-off-by: Ibrahim AshShohail <me@ibrasho.com>

---
+++ packages/eslint-config/README.md
@@ -0,0 +1,11 @@
+# `@mart-com/eslint-config`
+
+> TODO: description
+
+## Usage
+
+```
+const eslintConfigBase = require('@mart-com/eslint-config');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:prettier/recommended'],
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ['prettier', 'import'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'import/no-unresolved': [2, { commonjs: true, amd: true }],
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
+  },
+};
